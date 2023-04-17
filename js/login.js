cafe7257a5d4d7bdbd70687e57c24e870308d92d
Rosana <annros2003@gmail.com>
2023-04-17 18:40:16 +0300

    login is done

---
+++ js/login.js
@@ -0,0 +1,46 @@
+const URL = 'http://127.0.0.1:5000';
+
+function login(email, password) {
+    const url = `${URL}/user/login`;
+
+    fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json()
+        })
+        .then(data => {
+            localStorage.setItem('name', data.name);
+            localStorage.setItem('surname', data.surname);
+            localStorage.setItem('email', data.email);
+
+            if (data.status === "admin") {
+                window.location.href = "admin-profile.html";
+            } else if (data.status === "user") {
+                window.location.href = "index.html";
+            }
+
+        })
+        .catch(() => {
+            alert("Wrong credentials");
+        });
+}
+
+
+const loginForm = document.querySelector("form");
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    login(email, password);
+
+});
