04d013304f76ebbd0ec26eb47968f21576e8957d
rrossii <annros2003@gmail.com>
2023-05-20 23:56:47 +0300

    перенесла функції в utils

--- spa/src/components/AddEvent.js
+++ spa/src/components/AddEvent.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {useNavigate} from 'react-router-dom';
-import {isAdmin} from "../utils/Utils";
+import {isAdmin, categoriesArray, daysArray, hoursArray} from "../utils/Utils";
 
 export function AddEvent() {
     const [name, setName] = useState("");
@@ -22,44 +22,6 @@ export function AddEvent() {
     if (!isAdmin()) {
         return <h1 className={"m-5 text-center"}><b>Access denied.</b></h1>;
     }
-
-    const getDaysInMonth = (y, m) => {
-        const date = new Date(y, m, 1);
-        date.setMonth(date.getMonth() + 1);
-        date.setDate(date.getDate() - 1);
-        return date.getDate();
-    }
-
-    const daysArray = () => {
-        const daysInMonth = getDaysInMonth(year, month);
-        const days = []
-
-        for (let i = 1; i <= daysInMonth; i++) {
-            days.push(<option type="number" value={i}>{i}</option>);
-        }
-
-        return days;
-    }
-
-    const hoursArray = () => {
-        const hours = []
-        for (let i = 0; i <= 23; i++) {
-            hours.push(<option type="number" value={i}>{i}:00</option>);
-        }
-
-        return hours;
-    }
-
-    const categoriesArray = () => {
-        const categ = ["Theater", "Festival", "Sport", "Concert"];
-        const res = [];
-
-        for (let i = 0; i < categ.length; i++) {
-            res.push(<option type="string" value={categ[i]}>{categ[i]}</option>);
-        }
-        return res;
-    }
-
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -139,7 +101,7 @@ export function AddEvent() {
                                     <option value="11">December</option>
                                 </select>
                                 <select onChange={e => setDay(e.target.value)} value={day} className="day-dropdown" required>
-                                    {daysArray()}
+                                    {daysArray(year, month)}
                                 </select>
                                 <br/>
                                 <select onChange={e => setHour(e.target.value)}  className="hour-dropdown" placeholder="Hour" required>
--- spa/src/utils/Utils.js
+++ spa/src/utils/Utils.js
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import React from "react";
 
 export function isAdmin() {
     const user_status = localStorage.getItem("user_status" || "");
@@ -16,4 +16,40 @@ export function isUser() {
 export function isLoggedIn() {
     const user_session = localStorage.getItem("user_session");
     return user_session === "True";
+}
+
+export function categoriesArray() {
+    const categ = ["Theater", "Festival", "Sport", "Concert"];
+    const res = [];
+
+    for (let i = 0; i < categ.length; i++) {
+        res.push(<option type="string" value={categ[i]}>{categ[i]}</option>);
+    }
+    return res;
+}
+export function getDaysInMonth(y, m) {
+    const date = new Date(y, m, 1);
+    date.setMonth(date.getMonth() + 1);
+    date.setDate(date.getDate() - 1);
+    return date.getDate();
+}
+
+export function daysArray(year, month) {
+    const daysInMonth = getDaysInMonth(year, month);
+    const days = []
+
+    for (let i = 1; i <= daysInMonth; i++) {
+        days.push(<option type="number" value={i}>{i}</option>);
+    }
+
+    return days;
+}
+
+export function hoursArray() {
+    const hours = []
+    for (let i = 0; i <= 23; i++) {
+        hours.push(<option type="number" value={i}>{i}:00</option>);
+    }
+
+    return hours;
 }
