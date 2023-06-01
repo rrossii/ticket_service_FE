ae7a0131a00cf1f3b38f38dfc98c6bb81b6d26db
rrossii <annros2003@gmail.com>
2023-06-02 02:23:50 +0300

    Revert "corrected some things + login and footer tests"
    This reverts commit 5d74fe03cc01737bd2f99f99b2a8a379303daa29.

--- spa/src/setupTests.js
+++ spa/src/setupTests.js
@@ -3,19 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-beforeAll(() => {
-    // Mock the localStorage object
-    const localStorageMock = {
-        setItem: jest.fn(),
-        getItem: jest.fn(),
-    };
-
-    // Set the localStorage object globally
-    Object.defineProperty(window, "localStorage", { value: localStorageMock });
-});
-
-// jest.mock('react-router-dom', () => ({
-//     ...jest.requireActual('react-router-dom'),
-//         useNavigate: jest.fn(),
-// }));
